a530bac0be4e163527bce848ac7b126777d12432
Dewanand Vaidhya <vaidhya2@gmail.com>
2025-02-05 09:10:25 +0530

    remove cls

--- SystemDesignLLD/BehaviouralPatterns/Template Pattern/TemplatePattern.cs
+++ SystemDesignLLD/BehaviouralPatterns/Template Pattern/TemplatePattern.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace SystemDesignLLD.BehaviouralPatterns.Template_Pattern
 {
-    internal class TemplatePattern
-    {
-    }
+
 
     abstract class DataParser
     {
